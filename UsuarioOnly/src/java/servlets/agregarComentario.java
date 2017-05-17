ea7081be8a59b0fc377694e9fe92dd1b1fc31a2e
mzuluagag <mateo.zu.gi1999@gmail.com>
2017-05-16 21:42:08 -0500

    teo

--- UsuarioOnly/src/java/servlets/agregarComentario.java
+++ UsuarioOnly/src/java/servlets/agregarComentario.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//estecomentarioestamascuca
+
 package servlets;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
