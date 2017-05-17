e651274b6ec04c7ebdb18abc916b3a922503d847
saarboledaz <sergioarboledazuluaga@gmail.com>
2017-05-16 21:40:24 -0500

    gool

--- UsuarioOnly/src/java/servlets/agregarComentario.java
+++ UsuarioOnly/src/java/servlets/agregarComentario.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//estecomentarioestamascuca
 package servlets;
-//comentario de prueba
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
